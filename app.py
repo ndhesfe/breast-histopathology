e1ca63872414aa90c30194e7b641197045aecafa
ndhesfe <safitriindah0305@gmail.com>
2026-06-20 16:55:13 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Load TFLite model
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+labels = [
+    "Benign",
+    "Malignant"
+]
+
+st.title("Breast Cancer Histopathology Classification")
+
+uploaded_file = st.file_uploader(
+    "Upload gambar histopathology",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file).convert("RGB")
+
+    st.image(
+        image,
+        caption="Gambar yang diupload",
+        use_container_width=True
+    )
+
+    # Sesuaikan dengan ukuran training
+    img = image.resize((50, 50))
+
+    img = np.array(img, dtype=np.float32)
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    # Input ke model TFLite
+    interpreter.set_tensor(
+        input_details[0]['index'],
+        img
+    )
+
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(
+        output_details[0]['index']
+    )
+
+    pred_idx = np.argmax(prediction)
+    pred_class = labels[pred_idx]
+    confidence = float(np.max(prediction) * 100)
+
+    st.success(
+        f"Prediksi: {pred_class} | Confidence: {confidence:.2f}%"
+    )
+
+    st.write("Probabilitas:")
+    st.write(f"Benign: {prediction[0][0]*100:.2f}%")
+    st.write(f"Malignant: {prediction[0][1]*100:.2f}%")
